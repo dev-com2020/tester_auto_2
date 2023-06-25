4223e0ae81741dff2373ff095d3c34037a978307
kurs <kwp@tlen.pl>
2023-06-25 10:23:09 +0200

    10:22 25.06

---
+++ strukt2.py
@@ -0,0 +1,28 @@
+print(2 + 3 * 6 - 3)
+print(2 ** 3)
+print(2 + 5 * 6 / 2.2)
+print(2 + 5 * 6 // 2.2)
+print("Tomek " * 4)
+print([1,2,3] * 3)
+print("Tomek" + "Tomek")
+print("Tomek" , "Tomek")
+print([1,2,3] + [4,5,6])
+print([1,2,3] + ["Tomek","Asia"])
+
+x = 10
+y = 6
+
+z = x + y
+
+z = z + x
+z += x
+
+print(x > y)
+print(x < y)
+print(x == y)
+print(x != y)
+print(x <= y)
+print(x >= y)
+
+
+
